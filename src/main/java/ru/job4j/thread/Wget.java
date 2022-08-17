4e98f246cb26804aa1296a0133a096c1114753d7
BurlakovIvan <burlakovid@inbox.ru>
2022-08-17 10:27:29 +0300

    4. Скачивание файла с ограничением. [#144271 #333083]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -8,29 +8,40 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Wget implements Runnable {
 
     private static final int SIZE_BUFFER = 1024;
+    private static final int TIME_DOWNLOAD = 1000;
     private final String url;
+    private final String file;
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String file, int speed) {
         this.url = url;
+        this.file = file;
         this.speed = speed;
     }
 
     public static void validation(String[] args) {
-        if (args.length != 2) {
+        if (args.length != 3) {
             throw new IllegalArgumentException("Переданы не все параметры");
         }
         String url = args[0];
         if (!url.matches("^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")) {
             throw new IllegalArgumentException("Некорректный url адрес");
         }
+        File file = new File(args[1]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Не существует %s", file.getAbsoluteFile()));
+        }
+        if (file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Не является файлом %s", file.getAbsoluteFile()));
+        }
         int speed;
         try {
-            speed = Integer.parseInt(args[1]);
+            speed = Integer.parseInt(args[2]);
         } catch (NumberFormatException ex) {
             throw new IllegalArgumentException("Второй параметр не является числом");
         }
@@ -41,51 +52,44 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        try {
-            System.out.println("Start loading ... ");
-            File tempFile = Files.createTempFile(null, null).toFile();
-            try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-                 FileOutputStream fileOutputStream = new FileOutputStream(tempFile)) {
-                byte[] dataBuffer = new byte[SIZE_BUFFER];
-                int bytesRead;
-                long start = System.currentTimeMillis();
-                while ((bytesRead = in.read(dataBuffer, 0, SIZE_BUFFER)) != -1) {
-                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+        System.out.println("Start loading ... ");
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            byte[] dataBuffer = new byte[SIZE_BUFFER];
+            int bytesRead;
+            int downloadDate = 0;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, SIZE_BUFFER)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                downloadDate += bytesRead;
+                if (downloadDate >= speed) {
                     long finish = System.currentTimeMillis();
                     long diff = finish - start;
-                    if (SIZE_BUFFER > speed) {
-                        /*
-                        Сравниваем со скоростью 1024 б/сек, и скачиваем порциями по 1024 байта (SIZE_BUFFER)
-                        Если скорость скачивания задана меньше этого значения, то определяем время как
-                        1024/speed - узнаем во сколько раз скачали больше чем необходимо, то есть сколько миллисекунд
-                        шла бы загрузка при заданной скорости и из нее вычитаем уже затраченное время, если
-                        оно меньше чем вычисленное
-                         */
-                        long time = (SIZE_BUFFER * 1000) / speed;
-                        if (time > diff) {
-                            Thread.sleep(time - diff);
-                        }
+                    if (diff < TIME_DOWNLOAD) {
+                        Thread.sleep(TIME_DOWNLOAD - diff);
                     }
+                    downloadDate = 0;
                     start = System.currentTimeMillis();
                 }
-            } catch (IOException | InterruptedException ex) {
-                ex.printStackTrace();
             }
-        } catch (IOException ex) {
+        } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         validation(args);
+        long start = System.currentTimeMillis();
         Thread progress = new Thread(new ConsoleProgress());
         String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String file = args[1];
+        int speed = Integer.parseInt(args[2]);
+        Thread wget = new Thread(new Wget(url, file, speed));
         wget.start();
         progress.start();
         wget.join();
         progress.interrupt();
-        System.out.println("\nLoading is complete");
+        long finish = System.currentTimeMillis();
+        System.out.printf("\nLoading is complete. Time download : %d", (finish - start)/ TIME_DOWNLOAD);
     }
 }
